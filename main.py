d1948cffecb84f19aa3d436511f39d2ac0b580e1
jon-b-alves <alves.b.jon@gmail.com>
2025-04-17 15:51:24 -0400

    refactor: improved readabilty and code structure

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
 import shutil
 
-DOWNLOADS_PATH = r"C:\Users\jjohn\Downloads"
-count = 0
+def clean_downloads_directory():
+    user_name = os.getlogin()
+    downloads_path = f"C:/Users/{user_name}/Downloads"
+    count = 0
 
-for file in os.listdir(DOWNLOADS_PATH):
-    file_path = f"{DOWNLOADS_PATH}\{file}"
+    for file_system_object in os.listdir(downloads_path):
+        path = f"{downloads_path}/{file_system_object}"
+        
+        if os.path.isfile(path):
+            os.remove(path)
+            count += 1
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+            count += 1
+        else:
+            raise ValueError(f"{path} is neither a file nor directory")
 
-    if os.path.isfile(file_path):
-        os.remove(file_path)
-        count += 1
-    elif os.path.isdir(file_path):
-        shutil.rmtree(file_path)
-        count += 1
-    else:
-        raise ValueError(f"{file_path} is neither a file or directory")
+    print(f"Removed {count} file system objects from downloads directory")
 
-print(f"Removed {count} files/folders from downloads folder")
+
+if __name__ == "__main__":
+    clean_downloads_directory()
 
